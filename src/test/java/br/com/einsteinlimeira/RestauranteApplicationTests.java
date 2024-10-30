41f8a2099f7b8b00ca4a3c5b69db7e97ea0415a6
Gustavo <gustavo.ccx@hotmail.com>
2024-10-30 20:45:51 -0300

    INT-
    - Commit de Inicialização

---
+++ src/test/java/br/com/einsteinlimeira/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.einsteinlimeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
